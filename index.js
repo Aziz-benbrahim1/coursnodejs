851e149cca0412ca896bb820dca7415652f0b5c1
Aziz-benbrahim1 <azizbenbrahim1@icloud.com>
2025-02-11 02:40:38 +0100

    'aziz'

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const express= require('express')
-
+const path= require('path')
 const app=express()
 
-app.use((req,res,next)=>{
-   console.log('welcome from home')
-   next()
+
+app.set('view engine','ejs')
+app.set('views','views')
+
+
+
+app.use(express.static(path.join(__dirname,'assets')))
+
+
+app.get('/',(req,res,next)=>{
+   res.render('index',{name:'Aziz',lastname:'ben brahim'})
+   
+   
+   
 
 })
-app.use((req,res,next)=>{
-   console.log('welcome from 3abla')
-   next()
 
-})
-app.use((req,res)=>{
-   console.log('welcome 3chiri')
 
-})
+
 
 
 app.listen(3000,()=>console.log('server run on port 3000'))
