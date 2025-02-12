60dc9d53fbc9ceffc5496a4b692e86943e2da12f
Aziz-benbrahim1 <azizbenbrahim1@icloud.com>
2025-02-12 17:38:36 +0100

    'salem'

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express= require('express')
-const path= require('path')
 const app=express()
+const b=express.urlencoded({extended:true})
 
 
-app.set('view engine','ejs')
-app.set('views','views')
-
-
-
-app.use(express.static(path.join(__dirname,'assets')))
-
-
-app.get('/',(req,res,next)=>{
-   res.render('index',{name:'Aziz',lastname:'ben brahim'})
-   
-   
-   
-
+var students=[
+   {id:0,firstname:'aziz',lastname:'benbrahim',speciality:'programmer'},
+   {id:1,firstname:'salah',lastname:'blabla',speciality:'designer'},
+   {id:2,firstname:'mohamed',lastname:'benyoussef',speciality:'programmer'},
+   {id:3,firstname:'kamel',lastname:'mazghouni',speciality:'programmer'},
+]
+app.get('/',(req,res)=>{
+   res.send('home page')
 })
+
+app.get('/students',(req,res)=>{
+   res.send(students)
+})
+
+app.get('/student/:id',(req,res)=>{
+   let newstudent=students.find(student=>student.id==req.params.id)
+   if(newstudent){
+      res.send(newstudent)
+   }else{
+      res.send("we don't have this student")
+   }
+})
+
+app.get('/student/delete/:id',(req,res)=>{
+   let student=students.find(student=>student.id==req.params.id)
+   let positionstudent=students.indexOf(student)
+   students.splice(positionstudent,1)
+   res.send(students)
+})
+
+
+   
+   
+   
+
 
 
 
